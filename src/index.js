73be2cf12d2d49f432b337775320dcec20fca639
tesla0122 <hangochunga4@gmail.com>
2021-07-10 16:24:47 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+        app.set('view engine', 'hbs');
+     app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // route init
 route(app);
